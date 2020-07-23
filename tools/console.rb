34d02de7315a317f9ec5e149892b7ba929010dbe
Miqotes <ashleelennab@gmail.com>
2020-07-23 10:56:13 -0700

    Turning in code challenge

--- tools/console.rb
+++ tools/console.rb
@@ -6,13 +6,13 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-
-
-
-
-
-
-
+melissa = Author.new("Melissa")
+#
+chill_times = Magazine.new("Chill Times", "lifestyle")
+#
+#nice_piece = Article.new(@author, @magazine, "nice_piece")
+new_piece = Article.new(@author = "Sam", @magazine = "SFT", "new piece")
+old_times = Article.new(@author = "James", @magazine = "OT", "old_times")
 
 
 ### DO NOT REMOVE THIS
